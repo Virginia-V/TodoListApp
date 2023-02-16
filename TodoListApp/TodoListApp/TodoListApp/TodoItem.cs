f7ab83273d9eb1bc776214cab8723b23b7268680
Virginia Varzari <varzarivirginia4@gmail.com>
2023-02-16 20:00:45 +0200

    Initial Commit

---
+++ TodoListApp/TodoListApp/TodoListApp/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿namespace TodoListApp
+{
+    public class TodoItem
+    {
+        public string TodoText { get; set; }
+        public bool Complete { get; set; }
+        public TodoItem(string todoText, bool complete)
+        {
+            TodoText = todoText;
+            Complete = complete;
+        }
+    }
+}
---
+++ TodoListApp/TodoListApp/TodoListApp/TodoListViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace TodoListApp
+{
+    public class TodoListViewModel
+    {
+        public ObservableCollection<TodoItem> TodoItems { get; set; }
+
+        public TodoListViewModel()
+        {
+            TodoItems = new ObservableCollection<TodoItem>();
+            TodoItems.Add(new TodoItem("To do 1", false));
+            TodoItems.Add(new TodoItem("To do 2", true));
+            TodoItems.Add(new TodoItem("To do 3", false));
+        }
+
+        public ICommand AddTodoCommand => new Command(AddTodoItem);
+        public string NewTodoInputValue { get; set; }
+
+        void AddTodoItem()
+        {
+            TodoItems.Add(new TodoItem(NewTodoInputValue, false));
+        }
+
+        public ICommand RemoveTodoCommand => new Command(RemoveTodoItem);
+
+        void RemoveTodoItem(object o)
+        {
+            TodoItem todoItemBeingRemoved = o as TodoItem;  
+            TodoItems.Remove(todoItemBeingRemoved);
+        }
+    }
+}
